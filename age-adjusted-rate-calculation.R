75e0c39157f0b7a8e5c3e1d6337efedec07f55cd
nancyzhu24 <nancy.zhu.mcgill@gmail.com>
2018-06-20 13:52:01 -0400

    updates and add rmarkdown file

--- age-adjusted-rate-calculation.R
+++ age-adjusted-rate-calculation.R
@@ -2,6 +2,7 @@
 
 #calculate number of AS cases (from in hospital diagnosis) in each fsa area:
 case<-readRDS('case1.RData')
+
 case<-case%>%distinct(nam,dtsort,cyear)
 clsc<-readRDS('cleaned_clsc_terr.RData')%>%rename(clsc_rez=code_clsc)
 
@@ -41,3 +42,33 @@ final_tb$age.specific.rate[is.na(final_tb$age.specific.rate)]<-0
 #finally calculate age-standardized rate for each fsa code:
 age_standaridized<-final_tb%>%group_by(fsa_clean)%>%
                               summarise(age.standarized.rate=round(sum(age.specific.rate*proportion),1))
+
+
+########################################################################################3
+#now calculate age-standarized rate of AS surgical interventions:
+interv<-readRDS('interv.RData')
+
+interv<-interv%>%distinct(nam,dtsort,cyear)
+interv<-left_join(interv,clsc[,c(1,2,4)],by=c('nam'='nam','cyear'='year'))%>%
+  filter(!is.na(pc))
+interv<-left_join(interv,demo[,1:2])
+
+#calculate age at the time AS was first diagnosed in an individual
+#use year of diagnosis to calculate age (equivalent to rounding, birth date not available)
+interv$age_index<-interv$cyear-interv$year_born
+interv$age_index.cat<-cut(interv$age_index,breaks=c(seq(from = 60, to =85, by =5),Inf),right=F)
+
+interv_count<-interv%>%filter(pc %in% fsacode)%>%
+  group_by(pc,age_index.cat)%>%
+  summarise(interv=n_distinct(nam))
+
+interv_final_tb<-age_df3%>%filter(V5 %in% c('[60,65)','[65,70)','[70,75)','[75,80)','[80,85)','[85,Inf)'))%>%
+  left_join(interv_count,by=c('fsa_clean'='pc','V5'='age_index.cat'))%>%
+  left_join(standard_pop[,3:4])%>%
+  mutate(age.specific.rate=round(interv/population*1000,1))%>%
+  group_by(fsa_clean)%>%
+  mutate(proportion=population.std/sum(population.std))%>%
+  ungroup()%>%
+  mutate(age.specific.rate=ifelse(is.na(age.specific.rate),0,age.specific.rate))%>%
+  group_by(fsa_clean)%>%
+  summarise(age.standarized.rate=round(sum(age.specific.rate*proportion),1))
